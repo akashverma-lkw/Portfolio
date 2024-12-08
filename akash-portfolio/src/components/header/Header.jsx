4b15bd8ce185695a33500c62899875bb90d699e6
Akash Verma <akashvermastp7@gmail.com>
2024-12-09 02:36:10 +0530

    commit

--- akash-portfolio/src/components/header/Header.jsx
+++ akash-portfolio/src/components/header/Header.jsx
@@ -16,7 +16,7 @@ const Header = () => {
   return (
     <header className='header'>
         <nav className='nav container'>
-            <a href="index.html" className='nav__logo'>Akash</a>
+            <a href="index.html" className='nav__logo'>Akash Verma</a>
 
             <div className={Toggle ? "nav__menu show-menu" : 
                 "nav__menu"}>
