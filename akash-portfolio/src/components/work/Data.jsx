805b9e0b26f1857e91f59a21b1dfa543e50b7aa2
Akash Verma <akashvermastp7@gmail.com>
2025-04-30 12:01:23 +0530

    add resume builder

--- akash-portfolio/src/components/work/Data.jsx
+++ akash-portfolio/src/components/work/Data.jsx
@@ -3,6 +3,7 @@ import Work2 from '../../assets/work/work2.jpg.webp'
 import Work3 from '../../assets/work/work3.jpg'
 import Work4 from '../../assets/work4.jpg'
 import Work5 from '../../assets/work5.jpg'
+import Work6 from '../../assets/work6.jpg'
 
 export const projectsData = [
     {
@@ -22,20 +23,27 @@ export const projectsData = [
     },
     {
       id: 3,
+      image: Work6,
+      title: "Resume Builder",
+      category: "web",
+      link: "https://resume-builder-dusky-three.vercel.app"
+    },
+    {
+      id: 4,
       image: Work3,
       title: "Book Portal",
       category: "web",
       link: "https://book-portal-client.vercel.app",
     },
     {
-      id: 4,
+      id: 5,
       image: Work4,
       title: "App Development",
       category: "app",
 
     },
     {
-      id: 5,
+      id: 6,
       image: Work5,
       title: "Web design",
       category: "design",
