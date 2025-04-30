cf66aabe9164a167d03b2d552895c763c750837a
Akash Verma <akashvermastp7@gmail.com>
2025-04-30 12:06:26 +0530

    update

--- akash-portfolio/src/components/work/Data.jsx
+++ akash-portfolio/src/components/work/Data.jsx
@@ -3,7 +3,7 @@ import Work2 from '../../assets/work/work2.jpg.webp'
 import Work3 from '../../assets/work/work3.jpg'
 import Work4 from '../../assets/work4.jpg'
 import Work5 from '../../assets/work5.jpg'
-import Work6 from '../../assets/work6.jpg'
+import Work6 from '../../assets/work6.png'
 
 export const projectsData = [
     {
