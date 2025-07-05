0a23a42fe6279364baf3214a5af26fc13f0942bc
Akash Verma <akashvermastp7@gmail.com>
2025-07-05 20:40:26 +0530

    update files

--- akash-portfolio/src/components/work/Data.jsx
+++ akash-portfolio/src/components/work/Data.jsx
@@ -11,7 +11,7 @@ export const projectsData = [
       image: Work1,
       title: "Snapzy",
       category: "web",
-      link: "https://img.freepik.com/free-vector/abstract-coming-soon-halftone-style-background-design_1017-27282.jpg"
+      link: "https://snapzy-nu.vercel.app"
       
     },
     {
